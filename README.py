9e44eede5e6b88425b2a383c94b513131b33bf7a
NetworkChuckyoutubesubscriberfamily100 <149017543+NetworkChuckyoutubesubscriberfamily100@users.noreply.github.com>
2023-10-25 17:35:42 -0400

    Create README.py

---
+++ README.py
@@ -0,0 +1,4 @@
+#hello everyone im new to github butttttttt....... I LOVE TO CODE and im curruntly to learn pyhton, and im LOVING IIIIIIITTTTTTTT. P.s im 21 years old
+#my fav hobby is reading and im a big fan of Network chuck hes has so much fun and engaging videos about IT
+# for every one who is reading this go sub to network chuck
+# visit https://www.youtube.com/@NetworkChuck to see more and dont forget to follow him!!!!!
